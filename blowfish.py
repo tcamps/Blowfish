652ccbf7ca840ee5f38968334e9bcab7386f61d6
Toni Camps <44861005+tcamps@users.noreply.github.com>
2023-11-09 15:58:48 +0100

    Add files via upload

--- blowfish.py
+++ blowfish.py
@@ -36,6 +36,8 @@ enemic3.centery = HEIGHT / 2
 enemic3.velx = 5
 enemic3.vely = 5
 
+# Funció que mou el jugador en funció de les tecles presionades
+# Aquesta funció es cridada en cada frame
 def mou_jugador():
     if keyboard.LEFT and jugador.left > 0:
         jugador.centerx -= 5
@@ -46,6 +48,8 @@ def mou_jugador():
     if keyboard.DOWN and jugador.bottom < HEIGHT:
         jugador.centery += 5
 
+# Funció que mourà els enemics de forma automàtica
+# Aquesta funció és cridada en cada frame
 def mou_enemics():
     # Enemic 1
     if enemic1.left < 0 or enemic1.right > WIDTH:
@@ -71,13 +75,19 @@ def mou_enemics():
     enemic3.centerx += enemic3.velx
     enemic3.centery += enemic3.vely
 
+# Funció que posiciona el menjar en una posició aleatòria dins la pantalla
 def posicio_menjar():
     menjar.centerx = random.randint(0+round(menjar.width/2), WIDTH-round(menjar.width/2))
     menjar.centery = random.randint(0+round(menjar.height/2), HEIGHT-round(menjar.height/2))
 
+# La cridem un primer cop en començar el joc
 posicio_menjar()
 
+# Comença música
+music.play("island_meet_greet")
+
 def draw():
+    # Netegem pantalla dibuixant-la tota de blanc
     screen.fill((255, 255, 255))
 
     jugador.draw()
@@ -86,10 +96,14 @@ def draw():
     enemic2.draw()
     enemic3.draw()
 
-    screen.draw.text("Puntuació: " + str(puntuacio), left=5, top=5, color=(0, 0, 0), fontsize=25, fontname="gloria-hallelujah")
+    # Actualitzem el text de la puntuació a cada frame
+    screen.draw.text("Puntuació: " + str(puntuacio), left=5, top=5, color=(0, 0, 0), 
+        fontsize=25, fontname="gloria-hallelujah")
 
+    # Si s'ha produït el game over mostrem el text
     if game_over:
-        screen.draw.text("GAME OVER", centerx=WIDTH/2, centery=HEIGHT/2, color=(0, 0, 0), fontsize=80, fontname="gloria-hallelujah")
+        screen.draw.text("GAME OVER", centerx=WIDTH/2, centery=HEIGHT/2, color=(0, 0, 0), 
+            fontsize=80, fontname="gloria-hallelujah")
 
 def update():
     global puntuacio, game_over
@@ -97,14 +111,22 @@ def update():
     if game_over:
         return
     
-    # Col·lissions
+    # Col·lisió del jugador amb el menjar
     if jugador.colliderect(menjar):
         puntuacio += 1
+        # Reproduim so
+        sounds.menja_peix.play()
         posicio_menjar()
     
+    # Col·lisió del jugador amb un dels enemics
     if jugador.colliderect(enemic1) or jugador.colliderect(enemic2) or jugador.colliderect(enemic3):
         game_over = True
+        # Reproduim so
+        sounds.game_over.play()
+        # Aturem música del joc
+        music.stop()
 
+    # A cada frame cridem les funcions per moure el jugador i els enemics
     mou_jugador()
     mou_enemics()
 
